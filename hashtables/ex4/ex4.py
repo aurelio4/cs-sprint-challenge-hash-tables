e93c59482fc6b0052422037acf01da04390fb0f6
aurelio4 <aurelioarcabascio@gmail.com>
2020-07-10 14:27:24 -0400

    4 and 5 done

--- hashtables/ex4/ex4.py
+++ hashtables/ex4/ex4.py
@@ -3,9 +3,17 @@ def has_negatives(a):
     YOUR CODE HERE
     """
     # Your code here
+    storage = {}
+    result = []
+    
+    for num in a:
+        storage[num] = 1
+    
+    for num in a:
+        if (num*-1) in storage and num > 0:
+            result.append(num)
 
     return result
-
 
 if __name__ == "__main__":
     print(has_negatives([-1, -2, 1, 2, 3, 4, -4]))
--- hashtables/ex5/ex5.py
+++ hashtables/ex5/ex5.py
@@ -1,15 +1,24 @@
-# Your code here
-
-
-
 def finder(files, queries):
     """
     YOUR CODE HERE
     """
     # Your code here
+    result = []
+    cache = {}
+
+    for x in files:
+        key = x.rsplit('/', 1)[1]
+        value = x
+
+        if key not in cache:
+            cache[key] = []
+        cache[key].append(x)
+
+    for y in queries:
+        if y in cache:
+            result += cache[y]
 
     return result
-
 
 if __name__ == "__main__":
     files = [
